50112f0e3debaef68e7f3753f7101fec705b905a
poldown <poldown@98f46cc7-c239-0410-853f-79eade525fa0>
2007-09-14 10:55:35 +0000

    FreePolygon added

--- branches/PaintBrush/src/my/paintbrush/tools/DrawingTool.java
+++ branches/PaintBrush/src/my/paintbrush/tools/DrawingTool.java
@@ -8,11 +8,13 @@ import my.paintbrush.properties.Property;
 public enum DrawingTool {
 	NONE("<none>", "", "my.paintbrush.properties.Properties"),
 	LINE("Line", "my.paintbrush.tools.Line", "my.paintbrush.properties.BasicProperties"),
+	//ARC("Arc", "my.paintbrush.tools.Arc", "my.paintbrush.properties.BasicProperties"),
 	RECTANGLE("Rectangle", "my.paintbrush.tools.Rectangle", "my.paintbrush.properties.SimpleProperties"),
 	ROUNDRECTANGLE("Round Rectangle", "my.paintbrush.tools.RoundRectangle", "my.paintbrush.properties.RoundRectangleProperties"),
 	ELLIPSE("Ellipse", "my.paintbrush.tools.Ellipse", "my.paintbrush.properties.SimpleProperties"),
 	FREEDRAW("Free Drawing", "my.paintbrush.tools.FreeDraw", "my.paintbrush.properties.BasicProperties"),
-	SHAPES("Shapes", "my.paintbrush.tools.Shapes", "my.paintbrush.properties.ShapesProperties");
+	SHAPES("Shapes", "my.paintbrush.tools.Shapes", "my.paintbrush.properties.ShapesProperties"),
+	FREEPOLYGON("Free Polygon", "my.paintbrush.tools.FreePolygon", "my.paintbrush.properties.SimpleProperties");
 	
 	public String disName;
 	public String className;
